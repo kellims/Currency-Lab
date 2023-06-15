e823a4d24feb73dd91765a59a359de50a4c14e28
kellims <kellisimon18@gmail.com>
2023-06-14 19:48:43 -0600

    testing connection

--- currency_project/settings.py
+++ currency_project/settings.py
@@ -77,7 +77,8 @@ WSGI_APPLICATION = 'currency_project.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'world_currency',
+        'NAME': 'world_currency', 
+        
     }
 }
 
