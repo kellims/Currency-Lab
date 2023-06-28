ea86862f55fda31c886d295e29f200be5aab008a
kellims <kellisimon18@gmail.com>
2023-06-27 20:03:18 -0500

    home route working

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.shortcuts import render
+from django.views import View # <- View class to handle requests
+from django.http import HttpResponse
 
 # Create your views here.
+
+
+class Home(View):
+
+    def get(self, request):
+        return HttpResponse("Currency Home")
