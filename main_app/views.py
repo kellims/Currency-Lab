798f381ff3774677211e9bcfa7a51f5e26d69af3
kellims <kellisimon18@gmail.com>
2023-06-27 20:06:26 -0500

    added home template

--- main_app/views.py
+++ main_app/views.py
@@ -1,15 +1,14 @@
 from django.shortcuts import render
 from django.shortcuts import render
-from django.views import View # <- View class to handle requests
+from django.views import View #
 from django.http import HttpResponse
+from django.views.generic.base import TemplateView
 
 # Create your views here.
 
 
-class Home(View):
-
-    def get(self, request):
-        return HttpResponse("Currency Home")
+class Home(TemplateView):
+    template_name = "home.html"
     
 class About(View):
 
