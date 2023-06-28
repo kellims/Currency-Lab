128a98dbc711607c45fa3fa3c51f151065fcf9fd
kellims <kellisimon18@gmail.com>
2023-06-28 18:08:27 -0500

    currencies Showing

--- main_app/views.py
+++ main_app/views.py
@@ -38,5 +38,5 @@ class CurrencyList(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context["currency"] = currencies
+        context["currencies"] = currencies
         return context
