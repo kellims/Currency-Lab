b3a4c2ff63ee8c6b3d884d1804a3ea852de1834c
kellims <kellisimon18@gmail.com>
2023-06-28 18:55:35 -0500

    added new route

--- main_app/urls.py
+++ main_app/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.Home.as_view(), name="home"),
     path('about/', views.About.as_view(), name="about"),
     path('currency/', views.CurrencyList.as_view(), name="currency_list"),
+    path('currency/new/', views.CurrencyCreate.as_view(), name="currency_create"),
 ]
