77abc0881f1f901282de1494fca58eb08b63855d
kellims <kellisimon18@gmail.com>
2023-06-28 18:36:24 -0500

    seed data added to Django Admin

--- main_app/models.py
+++ main_app/models.py
@@ -6,7 +6,6 @@ class Currency(models.Model):
     name = models.CharField(max_length=100)
     image = models.CharField(max_length=500)
     country = models.TextField(max_length=50)
-    created_at = models.DateTimeField(auto_now_add=True)
     
     def __str__(self):
         return self.name
