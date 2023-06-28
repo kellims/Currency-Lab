474d1bea90cc5d3e7bfb0c8ad963575989c1941a
kellims <kellisimon18@gmail.com>
2023-06-28 18:22:11 -0500

    added Model and SuperUser

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Currency(models.Model):
+
+    name = models.CharField(max_length=100)
+    image = models.CharField(max_length=500)
+    country = models.TextField(max_length=50)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['name']
--- main_app/views.py
+++ main_app/views.py
@@ -30,7 +30,7 @@ currencies = [
     Currency("Chilean Peso", "https://www.foreigncurrencyandcoin.com/wp-content/uploads/2018/12/products-16032.jpg", "Chile" ),
     Currency("British Pound", "https://uploads-ssl.webflow.com/62bb7145ca6e491c926489aa/62ec46e0c8eb83e4d0889819_the-british-pound.webp", "UK" ),
     Currency("Euro", "https://en.numista.com/catalogue/photos/zone_euro/63b57a9bbfc8c0.69651644-original.jpg", "European Union" ),
-    Currency("Euro", "https://en.numista.com/catalogue/photos/zone_euro/63b57a9bbfc8c0.69651644-original.jpg", "European Union" ),
+
 ]        
 
 class CurrencyList(TemplateView):
