3cae8cabb40e33d78a09b64fe5e2a1411c307cab
kellims <kellisimon18@gmail.com>
2023-06-28 19:06:23 -0500

    new path added

--- main_app/urls.py
+++ main_app/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('about/', views.About.as_view(), name="about"),
     path('currency/', views.CurrencyList.as_view(), name="currency_list"),
     path('currency/new/', views.CurrencyCreate.as_view(), name="currency_create"),
+    path('currency/<int:pk>/', views.CurrencyDetail.as_view(), name="currency_detail"),
 ]
